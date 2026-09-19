5052db6e1107c26e46f8f3b052f4edeb48a94df6
ImFeH2 <i@feh2.im>
2026-09-19 22:00:37 +0800

    feat: add project scaffold

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import '@/styles/App.css'
+
+export default function App() {
+  return (
+    <main className="grid min-h-dvh place-items-center">
+      <h1 className="text-3xl font-semibold">Jev Arcade</h1>
+    </main>
+  )
+}
